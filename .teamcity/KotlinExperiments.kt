8ced9bdfeda893bdbc7636cc2ca8f4a8796498a8
clydedz <clyd94@gmail.com>
2021-05-25 22:00:32 +1200

    Move project into its own object

---
+++ .teamcity/KotlinExperiments.kt
@@ -0,0 +1,32 @@
+import _buildTypes.Build
+import _buildTypes.DeployToEnvironment
+import _dto.DeployToEnvironmentDto
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+
+object KotlinExperiments: Project({
+    val buildAndTest = Build()
+
+    val testDeploymentDto = DeployToEnvironmentDto(
+            env = "test",
+            storageAccountName = "craazstoragedemo48765",
+            manualDeployment = false,
+            triggeredByBuild = buildAndTest,
+            snapshotDependencyBuild = buildAndTest,
+            artifactDependencyBuild = buildAndTest
+    )
+    val testDeployment = DeployToEnvironment(testDeploymentDto)
+
+    val productionDeploymentDto = DeployToEnvironmentDto(
+            env = "production",
+            storageAccountName = "craazstoragedemo78050",
+            manualDeployment = true,
+            snapshotDependencyBuild = testDeployment,
+            artifactDependencyBuild = buildAndTest
+    )
+    val productionDeployment = DeployToEnvironment(productionDeploymentDto)
+
+    buildType(buildAndTest)
+    buildType(testDeployment)
+    buildType(productionDeployment)
+    buildTypesOrder = listOf(buildAndTest, testDeployment, productionDeployment)
+})
--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,32 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import _buildTypes.*
-import _dto.*
 
 version = "2020.2"
 
-project {
-    val build = Build()
-
-    val testDeploymentDto = DeployToEnvironmentDto(
-            env = "test",
-            storageAccountName = "craazstoragedemo48765",
-            manualDeployment = false,
-            triggeredByBuild = build,
-            snapshotDependencyBuild = build,
-            artifactDependencyBuild = build
-    )
-    val testDeployment = DeployToEnvironment(testDeploymentDto)
-
-    val productionDeploymentDto = DeployToEnvironmentDto(
-            env = "production",
-            storageAccountName = "craazstoragedemo78050",
-            manualDeployment = true,
-            snapshotDependencyBuild = testDeployment,
-            artifactDependencyBuild = build
-    )
-    val productionDeployment = DeployToEnvironment(productionDeploymentDto)
-
-    buildType(build)
-    buildType(testDeployment)
-    buildType(productionDeployment)
-}
+project(KotlinExperiments)
