a93ba23eb3a4fea76738040f23120d45132d01c0
clydedz <clyd94@gmail.com>
2021-05-29 15:44:52 +1200

    Update artifact rules

--- .teamcity/_buildTypes/Build.kt
+++ .teamcity/_buildTypes/Build.kt
@@ -12,6 +12,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 class Build : BuildType ({
     name = "Build & test"
 
+    artifactRules = "version.txt"
     publishArtifacts = PublishMode.SUCCESSFUL
 
     vcs {
