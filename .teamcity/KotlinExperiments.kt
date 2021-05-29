6977d45b895dea3b69a811abd54924d9f8428917
clydedz <clyd94@gmail.com>
2021-05-29 14:12:32 +1200

    Remove subproject id

--- .teamcity/KotlinExperiments.kt
+++ .teamcity/KotlinExperiments.kt
@@ -4,7 +4,6 @@ import _dto.DeployToEnvironmentDto
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 
 object KotlinExperiments: Project({
-    id("KotlinExperiments")
     val buildAndTest = Build()
 
     val testDeploymentDto = DeployToEnvironmentDto(
